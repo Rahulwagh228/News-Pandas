944503dbb703c5730b57998cbd715b732f481a0a
Rahulwagh228 <127666835+Rahulwagh228@users.noreply.github.com>
2023-07-31 02:58:16 +0530

    half completed

--- src/Components/Newsitem.js
+++ src/Components/Newsitem.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react'
 
 export class Newsitem extends Component {
   render() {
+    let {title, description, urlToImage, newsurl} = this.props;
     return (
-      <div>This is newsitem compoenent</div>
+      <div>
+            <div className="card" style={{width: "18rem"}}>
+      <img src={!urlToImage?"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_E21EbyIpLutzSoftgE3K5eiKQOcUsqDsog&usqp=CAU":urlToImage} className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">{title}..</h5>
+        <p className="card-text">{description}..</p>
+        <a href={newsurl} className="btn btn-sm btn-primary">Read More </a>
+      </div>
+</div>
+      </div>
     )
   }
 }
--- src/Components/news.js
+++ src/Components/news.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react'
-import Newsitem from './Newsitem'
+import React, { Component } from 'react';
+import Newsitem from './Newsitem';
 
-export class news extends Component {
+export class News extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      articles: [], 
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=3386ba9149d84d12b48a41d9ca4b825f";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    console.log(parseData);
+    this.setState({ articles: parseData.articles });
+  }
+
   render() {
     return (
-      <div>
-        This is news item
+      <div className='container my-4'>
+        <h1>NewsPandas :- Top Headlines</h1>
+        <div className='row'>
+          {this.state.articles.map((element) => {
+            return (
+              <div className='col-md-4' key={element.url}>
+                <Newsitem
+                  title={element.title?element.title.slice(0,44):""}
+                  description={element.description?element.description.slice(0.88):""}
+                  urlToImage={element.urlToImage} 
+                  newsurl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
         <Newsitem/>
       </div>
-    )
+    );
   }
 }
 
-export default news
+export default News;
